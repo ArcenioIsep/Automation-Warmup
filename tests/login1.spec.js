a0197cf27c537eb0217bfc898bcb5711786c8b81
unknown <1171351@isep.ipp.pt>
2025-11-16 19:44:49 -1200

    Initial implementati  refactory  with the advace challage

--- tests/login1.spec.js
+++ tests/login1.spec.js
@@ -1,12 +1,62 @@
-import test, { expect } from "@playwright/test";
+import { test, expect } from '@playwright/test';
+import { USERS } from './data/test.data';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('/login'); // 👉 /login é adicionado ao baseURL
+});
+
+test('Login successful', async ({ page }) => {
+  await page.getByRole('textbox', { name: 'Type your username' }).fill(USERS.valid.username);
+  await page.getByRole('textbox', { name: 'Type your password' }).fill(USERS.valid.password);
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByText(USERS.valid.expectedMessage)).toBeVisible();
+
+});
+
+/**
+ * now we gonna go to the 2 scenrio to check  block acconnt 
+ */
+test('Block account ', async ({ page }) => {
+  await page.getByRole('textbox', { name: 'Type your username' }).fill(USERS.blocked.username);
+  await page.getByRole('textbox', { name: 'Type your password' }).fill(USERS.blocked.password);
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByText(USERS.blocked.expectedMessage)).toBeVisible();
+});
 
 
-test('Login sucessful', async ({page})=>{
+test('Invalid user', async ({ page }) => {
+  await page.getByRole('textbox', { name: 'Type your username' }).fill(USERS.invalid.username);
+  await page.getByRole('textbox', { name: 'Type your password' }).fill(USERS.invalid.password);
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByText(USERS.invalid.expectedMessage)).toBeVisible();
+});
 
-    await page.goto('https://playground-drab-six.vercel.app/login');
-    await page.getByRole('textbox', {name: 'Type your username'}).fill('test');
-    await page.getByRole('textbox', {name: 'Type your password'}).fill('password123');
-    await page.getByRole('button', { name: 'Login' }).click();
+test('Wrong password', async ({ page }) => {
+  await page.getByRole('textbox', { name: 'Type your username' }).fill(USERS.wrongPassword.username);
+  await page.getByRole('textbox', { name: 'Type your password' }).fill(USERS.wrongPassword.password);
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByText(USERS.wrongPassword.expectedMessage)).toBeVisible();
+});
 
-    await expect(page.getByText('User successfully logged in!')).toBeVisible();
+
+test('Wrong password 3 times (temporary block)', async ({ page }) => {
+ // await page.goto('https://playground-drab-six.vercel.app/login');
+  // 1ª tentativa
+  await page.getByRole('textbox', { name: 'Type your username' }).fill(USERS.temporaryBlock.username);
+  await page.getByRole('textbox', { name: 'Type your password' }).fill(USERS.temporaryBlock.password);
+  await page.getByRole('button', { name: 'Login' }).click();
+  
+  // 2ª tentativa
+  await page.getByRole('textbox', { name: 'Type your username' }).fill(USERS.temporaryBlock.username);
+  await page.getByRole('textbox', { name: 'Type your password' }).fill(USERS.temporaryBlock.password);
+  await page.getByRole('button', { name: 'Login' }).click();
+
+
+  // 3ª tentativa
+  await page.getByRole('textbox', { name: 'Type your username' }).fill(USERS.temporaryBlock.username);
+  await page.getByRole('textbox', { name: 'Type your password' }).fill(USERS.temporaryBlock.password);
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  
+  await expect(page.getByText(USERS.temporaryBlock.expectedMessage)).toBeVisible();
 });
